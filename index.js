9acafa004964d7e0efbe48830975558ad1b0696e
George Baroi <baroi.george@gmail.com>
2022-07-19 16:52:39 -0400

    commit tree f8c0e1e765efe78adea481d7277b1eb982a65115

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+//implement a function called saturdayFun. It should return a String like "This Saturday, I want to ....!" Fill in the ... with the activity that's passed in as the first parameter. If nothing is passed in, default to "roller-skate"
+ function saturdayFun(activity= 'roller-skate'){
+ return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = "*"){
+    return function(param="special"){
+        return `You are ${flair}${param}${flair}!`
+    }
+}
